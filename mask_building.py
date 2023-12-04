9141b6f6901759af72d26ff6f7e25adc436eb22c
Soskiiit <NikitaA439@gmail.com>
2023-12-05 01:47:05 +0300

    +exceptions

--- mask_building.py
+++ mask_building.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import shutil
 
 from PIL import Image
 import cv2
@@ -8,6 +9,7 @@ import torchvision
 
 
 def build_masks():
+    exceptions_list = os.listdir("./exceptions")
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     unet = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet',
         in_channels=3, out_channels=1, init_features=32, pretrained=False)
@@ -27,12 +29,15 @@ def build_masks():
     transform_to_img = torchvision.transforms.ToPILImage()
     transform_to_tensor = torchvision.transforms.ToTensor()
     for filename in os.listdir(in_test_path):
-        img = cv2.imread(str(in_test_path / filename))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        tensor = transform_to_tensor(img).to(device)
-        pred = unet(tensor.unsqueeze(0)).squeeze(0)
-        mask = transform_to_img(pred)
-        mask.save(out_test_masks_path / filename)
+        if filename not in exceptions_list:
+            img = cv2.imread(str(in_test_path / filename))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            tensor = transform_to_tensor(img).to(device)
+            pred = unet(tensor.unsqueeze(0)).squeeze(0)
+            mask = transform_to_img(pred)
+            mask.save(out_test_masks_path / filename)
+        else:
+            shutil.copy(f"./exceptions/{filename}", str(out_test_masks_path / filename))
 
 
 if __name__ == "__main__":
